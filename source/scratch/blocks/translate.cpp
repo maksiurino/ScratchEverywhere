8451c43e0168bfa9fa864868a2308592b1ffd97b
maksiurino <153639807+maksiurino@users.noreply.github.com>
2025-10-01 09:31:07 +0200

    Create translate.cpp

---
+++ source/scratch/blocks/translate.cpp
@@ -0,0 +1,13 @@
+#include "translate.hpp"
+#include "interpret.hpp"
+#include "sprite.hpp"
+#include "value.hpp"
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <math.h>
+
+Value TranslateBlocks::translate(Block &block, Sprite *sprite) {
+  Value value1 = Scratch::getInputValue(block, "WORDS", sprite);
+  Value value2 = Scratch::getInputValue(block, "LANGUAGE", sprite);
+}
